6aebf1d9e44b4c9f6b5b166ca59167955169e012
Netrajyoti <95840443+Netrajyoti@users.noreply.github.com>
2021-12-09 16:42:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,3 +150,5 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+document.getElementById("date").innerHTML = Date();
